40ab0b44e8a4c5199d50022cffda26530243fc06
Valerii Voronkov <scorpirat@gmail.com>
2021-01-30 18:55:08 +0300

    Create basic structure for index.php.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Doodle</title>
+</head>
+<body>
+    <div class="main-section">
+        <div class="logo-container">
+            <img src="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_92x36dp.png" 
+                alt="doogle logo" 
+                srcset="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_92x36dp.png">
+        </div>
+
+        <div class="search-container">
+            <input type="text" class="search-box" name="term">
+            <input type="submit" class="search-button" value="Search" placeholder="Enter a search text">
+        </div>
+    </div>
+</body>
+</html>
