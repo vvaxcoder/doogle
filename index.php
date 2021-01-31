089f6ed98f815c1784f57cccf2ccf91e42d820f6
Valerii Voronkov <scorpirat@gmail.com>
2021-01-31 13:42:28 +0300

    Add styles for index and search pages.

--- index.php
+++ index.php
@@ -8,19 +8,22 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <div class="main-section">
-        <div class="logo-container">
-            <img src="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_92x36dp.png" 
-                alt="doogle logo" 
-                srcset="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_92x36dp.png">
-        </div>
+    <div class="wrapper index-page">
+        <div class="main-section">
+            <div class="logo-container">
+                <img src="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_92x36dp.png" 
+                    alt="doogle logo" 
+                    srcset="https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_92x36dp.png">
+            </div>
 
-        <div class="search-container">
-            <form action="search.php" method="GET">
-                <input type="text" class="search-box" name="term">
-                <input type="submit" class="search-button" value="Search" placeholder="Enter a search text">
-            </form>
+            <div class="search-container">
+                <form action="search.php" method="GET">
+                    <input type="text" class="search-box" name="term">
+                    <input type="submit" class="search-button" value="Search" placeholder="Enter a search text">
+                </form>
+            </div>
         </div>
     </div>
+
 </body>
 </html>
