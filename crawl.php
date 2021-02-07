b7138e97bca1d77cabe97281598629cd07ee1cd3
Valerii Voronkov <scorpirat@gmail.com>
2021-02-07 22:29:47 +0300

    Converting relative links to absolute links.

--- crawl.php
+++ crawl.php
@@ -1,7 +1,28 @@
 <?php
     include("classes/DomDocumentParser.php");
 
-    function createLink($src, $url) {}
+    function createLink($src, $url) {
+        $scheme = parse_url($url)["scheme"];
+        $host = parse_url($url)["host"];
+        
+        if(substr($src, 0, 2) == "//") {
+            $src =  $scheme . ":" . $src;
+        }
+        else if(substr($src, 0, 1) == "/") {
+            $src = $scheme . "://" . $host . $src;
+        }
+        else if(substr($src, 0, 2) == "./") {
+            $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+        }
+        else if(substr($src, 0, 3) == "../") {
+            $src = $scheme . "://" . $host . "/" . $src;
+        }
+        else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+            $src = $scheme . "://" . $host . "/" . $src;
+        }
+
+        return $src;
+    }
 
     function followLinks($url) {
         $parser = new DomDocumentParser($url);
@@ -17,8 +38,8 @@
                 continue;
             }
     
-            createLink($href, $url);
-            // echo $href . "<br>";
+            $href = createLink($href, $url);
+            echo $href . "<br>";
         }
     }
 
